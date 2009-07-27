c16a1d145ef11a7a6f0ed63ee143bc4c52d87ad2
ofri <ofri.raviv@gmail.com>
2009-07-27 13:59:31 +0300

    updated the visualize template to work with the base templates

--- ecclesia/goals/views.py
+++ ecclesia/goals/views.py
@@ -1,3 +1,4 @@
+# This Python file uses the following encoding: utf-8
 from ecclesia.goals.models import *
 from ecclesia.operations.models import *
 
@@ -14,8 +15,8 @@ def visualize(request,goal_id):
 
 def json(request,goal_id):
     g = get_object_or_404(Goal, pk=goal_id)
-    res = """{"goal": {"id": %d, "name": "%s","short_description": "%s"},     
-              "actions": [{"id":1,"name":"a1"},{"id":2,"name":"a2"},{"id":3,"name":"a3"}],
+    res = u"""{"goal": {"id": %d, "name": "%s","short_description": "%s"},     
+              "actions": [{"id":1,"name":"a1"},{"id":2,"name":"action2"},{"id":3,"name":"action with longer name"}],
               "results": [{"id":1,"name":"r1"},{"id":2,"name":"r2"},{"id":3,"name":"r3"},{"id":4,"name":"r4"}],
               "a2r": [{"from":1,"to":1},{"from":1,"to":1},{"from":2,"to":2},{"from":2,"to":3},{"from":3,"to":4}],
               "r2g": [{"from":1},{"from":2},{"from":3},{"from":4}]  }""" % (g.id, g.name, g.short_description)
