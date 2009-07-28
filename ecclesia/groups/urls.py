8a2ecd38dfb732637d28999fc02778e7489fb630
Alexander Arshavski <alex@Macintosh-6.local>
2009-07-29 00:26:05 +0300

    modified:   discussion/models.py 	modified:   groups/urls.py 	modified:   groups/views.py 	new file:   static/js/jquery/css/ui.all.css 	new file:   static/js/jquery/css/ui.base.css 	new file:   static/js/jquery/css/ui.core.css 	new file:   static/js/jquery/css/ui.dialog.css 	new file:   static/js/jquery/css/ui.resizable.css 	new file:   static/js/jquery/css/ui.theme.css 	new file:   static/js/jquery/ui/ui.core.js 	new file:   static/js/jquery/ui/ui.dialog.js 	new file:   static/js/jquery/ui/ui.draggable.js 	new file:   static/js/jquery/ui/ui.resizable.js 	new file:   templates/admin/base.html

--- ecclesia/groups/urls.py
+++ ecclesia/groups/urls.py
@@ -3,4 +3,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^$', 'groups.views.home'),
+    (r'^group/is_in_group/$', 'groups.views.is_in_group'),
+    (r'^group/join_group/$', 'groups.views.join_group'),
+    (r'^group/leave_group/$', 'groups.views.leave_group'),
 )
--- ecclesia/groups/views.py
+++ ecclesia/groups/views.py
@@ -3,10 +3,29 @@ from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from groups.models import *
-
+from django.contrib.auth.models import Group
 
 
 def home(request):
     """Renders the application home page"""
     groups = GroupProfile.objects.all()
     return render_to_response('home.html', locals())
+
+def is_in_group(request):
+    if 'group_id' in request.GET:
+        group = Group.objects.get(id=request.GET['group_id'])
+        if request.user.groups.filter(id=group.id):
+            return HttpResponse("True")
+    return HttpResponse("False")
+    
+def join_group(request):
+    if 'group_id' in request.POST:
+        group = Group.objects.get(id=request.POST['group_id'])
+        request.user.groups.add(group)
+    return HttpResponse("")
+    
+def leave_group(request):
+    if 'group_id' in request.POST:
+        group = Group.objects.get(id=request.POST['group_id'])
+        request.user.groups.remove(group)
+    return HttpResponse("")
