579e0781d85391c7f76b05f8c1b49a29780262fd
alexarsh <alexarsh5@gmail.com>
2010-12-13 20:52:48 +0200

    returning django csrf to the old fashion till django 1.4.

--- ecclesia/settings.py
+++ ecclesia/settings.py
@@ -92,7 +92,7 @@ TEMPLATE_LOADERS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.csrf.middleware.CsrfMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 )
