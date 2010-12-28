4e3bbe7e99edafdd496875eb463aebee6390d0dc
Udi h Bauman <dibaunaumh@gmail.com>
2010-12-28 23:13:51 +0200

    Added initial hints

---
+++ ecclesia/discussions/coa_workflow_hints.py
@@ -0,0 +1,70 @@
+from workflow_hints import *
+
+
+
+# CoA workflow status hints
+WORKFLOW_HINTS = {
+    ADD_GOALS: {
+        TEXT: _("Please set your Goals: right-click the Goals column and choose 'Add story'"),
+        HELP_LINK: "",
+        POSITION: "goal",
+        ICON: "",
+    },
+
+    ADD_CONDITIONS: {
+        TEXT: _("Please add Conditions for reaching the Goals: right-click the Conditions column and choose 'Add story'"),
+        HELP_LINK: "",
+        POSITION: "condition",
+        ICON: "",
+    },
+
+    ADD_RELATIONS_FROM_CONDITIONS_TO_GOALS: {
+        TEXT: _("Please add relations from Conditions to Goals: right-click the Conditions and choose 'Add relation'"),
+        HELP_LINK: "",
+        POSITION: "condition",
+        ICON: "",
+    },
+
+    ADD_OPTIONS: {
+        TEXT: _("Please set Options for reaching the Goals: right-click the Options column and choose 'Add story'"),
+        HELP_LINK: "",
+        POSITION: "option",
+        ICON: "",
+    },
+
+    ADD_EFFECTS: {
+        TEXT: _("Please describe the Effects of each Option: right-click the Effects column and choose 'Add story'"),
+        HELP_LINK: "",
+        POSITION: "effect",
+        ICON: "",
+    },
+
+    ADD_RELATIONS_FROM_OPTIONS_TO_EFFECTS: {
+        TEXT: _("Please add relations from Options to Effects: right-click the Options and choose 'Add relation'"),
+        HELP_LINK: "",
+        POSITION: "option",
+        ICON: "",
+    },
+
+    ADD_OPINIONS_ON_STORIES: {
+        TEXT: _("Please describe your opinion on each story: Right-click a story and choose 'Add opinion'"),
+        HELP_LINK: "",
+        POSITION: "top",
+        ICON: "",
+    },
+
+    ADD_OPINIONS_ON_RELATIONS: {
+        TEXT: _("Please describe your opinion on each relation: Right-click a relation and choose 'Add opinion'"),
+        HELP_LINK: "",
+        POSITION: "top",
+        ICON: "",
+    },
+
+    START_VOTE: {
+        TEXT: _("You can now start a Voting session: Click the 'Start vote' button"),
+        HELP_LINK: "",
+        POSITION: "vote",
+        ICON: "",
+    },
+    
+}
--- ecclesia/discussions/workflow_hints.py
+++ ecclesia/discussions/workflow_hints.py
@@ -2,26 +2,20 @@ from coa_workflow_manager import *
 from django.utils.translation import ugettext as _
 
 # hint metadata fields
+import coa_workflow_hints
+from django.template.loader import render_to_string
+
 TEXT = "text"
 HELP_LINK = "help_link"
-DOM_ELEMENT = "dom_element"
-RELEVANT_POSITION = "relevant_position"
+POSITION = "position"
 ICON = "icon"
 
 
+def get_workflow_hints(discussion):
+    if discussion.type.name == "course-of-action":
+        metadata = coa_workflow_hints.WORKFLOW_HINTS
+        for md in metadata:
+            md["hint_html"] = render_to_string("hint.html", md)
+        return metadata
 
-
-# workflow status hints
-WORKFLOW_HINTS = {
-    ADD_GOALS: {
-        TEXT: _("Please add "),
-        HELP_LINK: "",
-        DOM_ELEMENT: "",
-        RELEVANT_POSITION: "",
-        ICON: "",
-    },
-
-
-
-
-}
+    return None
