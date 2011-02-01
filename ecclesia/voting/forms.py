83087d2fb399a2610aba58ce4e8bd4c57ee67260
alexarsh <alexarsh5@gmail.com>
2011-02-01 21:54:58 +0200

    fixing bug that you can create voting with time 0.

--- ecclesia/voting/forms.py
+++ ecclesia/voting/forms.py
@@ -6,3 +6,11 @@ class VotingForm(forms.Form):
     days = forms.IntegerField(min_value=0, required=False)
     hours = forms.IntegerField(min_value=0, max_value=24, required=False)
     minutes = forms.IntegerField(min_value=0, max_value=59, required=False)
+
+    def clean(self):
+        if self.cleaned_data['days'] == 0 and self.cleaned_data['hours'] == 0 \
+                and self.cleaned_data['minutes'] == 0:
+            message = "Voting time can't be 0"
+            raise forms.ValidationError, message
+        else:
+            return super(VotingForm, self).clean()
