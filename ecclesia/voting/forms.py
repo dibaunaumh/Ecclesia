7193ac6274338652ac2a0fc5c4da53b2f0b7dbd0
alexarsh <alexarsh5@gmail.com>
2011-02-05 16:03:23 +0200

    fixed bug with 0 votes per participant

--- ecclesia/voting/forms.py
+++ ecclesia/voting/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from models import Voting
 
 class VotingForm(forms.Form):
-    votes_per_participant = forms.IntegerField(min_value=0)
+    votes_per_participant = forms.IntegerField(min_value=1)
     days = forms.IntegerField(min_value=0, required=False)
     hours = forms.IntegerField(min_value=0, max_value=24, required=False)
     minutes = forms.IntegerField(min_value=0, max_value=59, required=False)
