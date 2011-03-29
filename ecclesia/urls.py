399ed87115fdd864daf6b64f2f5df4355f668cc3
alexarsh <alexarsh5@gmail.com>
2011-03-29 22:46:14 +0200

    moving to django 1.3

--- ecclesia/urls.py
+++ ecclesia/urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     #(r'^admin/', include(admin.site.urls)),
-    (r'^admin/(.*)', admin.site.root),
+    (r'^admin/', include(admin.site.urls)),
 
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'static'}), # serve static content. only for development.
 
