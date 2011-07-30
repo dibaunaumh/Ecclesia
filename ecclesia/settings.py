8cfe705df403fe0b03a32a73fe84ee728e2f594c
Alex <alexarsh5@gmail.com>
2011-07-30 17:57:32 +0300

    fixing settings.py after Vlad's commit

--- ecclesia/settings.py
+++ ecclesia/settings.py
@@ -18,8 +18,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'ekkli',                      # Or path to database file if using sqlite3.
         'USER': 'postgres',                      # Not used with sqlite3.
-        'PASSWORD': 'admin',                  # Not used with sqlite3.
-        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PASSWORD': 'ekkli',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
